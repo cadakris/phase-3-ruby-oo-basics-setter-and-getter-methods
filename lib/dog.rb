20081869f2734ee9fbd92680f5894a21c5c0e400
Kristen <kriscadacio@gmail.com>
2022-04-04 22:00:57 -0400

    commit tree 6ee8fb72eb2c170f1f639b3fbc6ea5487e61565f

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
 
+    def name
+        @name
+    end
+
+    def name = (name)
+        @name = name
+    end
+
+    def breed
+        @nbreed
+    end
+
+
+    def breed = (breed)
+        @breed = breed
+    end
+
+
+
 end
